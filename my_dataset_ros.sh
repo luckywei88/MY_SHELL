a4155da866d877ca1becc0db53e8dcca6dae3dd9
luckywei88 <1010224593@qq.com>
2018-05-05 13:06:08 +0800

    upload

--- my_dataset_ros.sh
+++ my_dataset_ros.sh
@@ -15,7 +15,7 @@ Yolo_Cfg=/home/lucky/MyLib/darknet/cfg/high-tiny-yolo.cfg
 
 Launch=/home/lucky/workspace/launch/bag_orb_slam.launch
 Send=/home/lucky/workspace/launch/send_dataset.launch
-Dir=/home/lucky/dataset/TUM/
+Dir=/home/lucky/dataset/my_dataset/
 
 rm -rf /home/lucky/pcd/tmppcd/*
 rm bag.log
@@ -37,19 +37,11 @@ read input
 
 DataSet=${array[$input]}
 
-n=${DataSet:21:1}
-
-
-#Config=$(pwd)/Freiburg${n}_low.yaml
-Config=$(pwd)/Freiburg${n}.yaml
+Config=$(pwd)/high_xtion.yaml
 
 {
-	sleep 11
-	roslaunch $Send dataset:=TUM sequence:=$DataSet time:=$Rate
-}&
-
-{
-	rosrun rviz rviz -d /home/lucky/.rviz/record.rviz
+	sleep 9
+	roslaunch $Send dataset:=my_dataset sequence:=$DataSet time:=$Rate
 }&
 
 roslaunch $Launch config:=$Config yolo_cfg:=$Yolo_Cfg yolo_weights:=$Yolo_Weights yolo_data:=$Yolo_Data yolo_label:=$Yolo_Label > bag.log
